ea983ab6304b99288b219fb8e116defd8f6864bb
thejuss <thejuss@qburst.com>
2023-11-06 22:57:49 +0530

    Fix the styling issue in action links

--- src/components/actionLinks/actionLinks.style.ts
+++ src/components/actionLinks/actionLinks.style.ts
@@ -4,64 +4,54 @@ export const LinkWrapper = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 30px;
-  height: 92px;
-  /* border: 1px solid #fff; */
-  .linkBox {
-    border-top: 1px solid #fff;
-    width: 144px;
-    padding: 7px 0;
-    text-align: center;
-    margin-right: 29px;
-    transition: border ease 2s;
-    cursor: pointer;
-    .label {
-      font-size: 15.219px;
-      font-weight: 700;
-      line-height: 18px; /* 118.273% */
-      letter-spacing: -1px;
-    }
-  }
-  .linkBox:last-child {
-    margin-right: 0;
-  }
-
-  .linkBox:hover {
-    border-top: 2px solid #fff;
-    border-bottom: 1px solid #fff;
-  }
+  height: 50px;
 `;
 
 export const LinkBox = styled.div`
-  border-top: 1px solid #fff;
+  position: relative;
   width: 144px;
-  height: fit-content;
-  padding: 7px 0;
+  height: 100%;
   text-align: center;
   margin-right: 29px;
+  border-top: 1px solid #fff;
 
   cursor: pointer;
   .label {
+    position: absolute;
+    top: 10px;
+    left: 50%;
+    transform: translateX(-50%);
     font-size: 15.219px;
     font-weight: 700;
     line-height: 18px; /* 118.273% */
     letter-spacing: -1px;
   }
-  .subLabel{
+  .hoverBoxLayout {
+    height: 0;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    padding: 5px 0;
+    transition: all 1000ms cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  .subLabel {
     font-size: 14px;
     font-weight: 300;
-    margin-top: 5px;
-    display: none;
+    margin-top: -10px;
+    opacity: 0;
+    transition: all 1000ms cubic-bezier(0.075, 0.82, 0.165, 1);
   }
 
-  /* .linkBox:last-child {
-    margin-right: 0;
-  } */
-
   &:hover {
-    border-top: 2px solid #e6292e;
-    border-bottom: 1px solid #e6292e;
-    .subLabel{
-        display: block;
+    border: none;
+    .hoverBoxLayout {
+      height: 100%;
+      border-top: 2px solid #e6292e;
+      border-bottom: 1px solid #e6292e;
+    }
+    .subLabel {
+      margin-top: 25px;
+      opacity: 1;
     }
   }
 `;
--- src/components/actionLinks/actionLinks.tsx
+++ src/components/actionLinks/actionLinks.tsx
@@ -1,22 +1,28 @@
-import { LinkBox, LinkWrapper } from "./actionLinks.style"
+import { LinkBox, LinkWrapper } from "./actionLinks.style";
 
-function ActionLinks(){
-    return(
-        <LinkWrapper>
-            <LinkBox>
-                <p className="label">Rules</p>
-                <p className="subLabel">See all rules</p>
-            </LinkBox>
-            <LinkBox>
-                <p className="label">Leaderboard</p>
-                <p className="subLabel">List of the best!</p>
-            </LinkBox>
-            <LinkBox>
-                <p className="label">Register</p>
-                <p className="subLabel">Save your spot</p>
-            </LinkBox>
-        </LinkWrapper>
-    )
+function ActionLinks() {
+  return (
+    <LinkWrapper>
+      <LinkBox>
+        <p className="label">Rules</p>
+        <div className="hoverBoxLayout">
+          <p className="subLabel">See all rules</p>
+        </div>
+      </LinkBox>
+      <LinkBox>
+        <p className="label">Leaderboard</p>
+        <div className="hoverBoxLayout">
+          <p className="subLabel">List of the best!</p>
+        </div>
+      </LinkBox>
+      <LinkBox>
+        <p className="label">Register</p>
+        <div className="hoverBoxLayout">
+          <p className="subLabel">Save your spot</p>
+        </div>
+      </LinkBox>
+    </LinkWrapper>
+  );
 }
 
-export default ActionLinks
+export default ActionLinks;
