8df95188303715150cbc34a383767354b9e933be
thejuss <thejuss@qburst.com>
2023-11-12 23:24:48 +0530

    Add new assets and set new background

--- src/components/backgroundAnimation/backgroundAnimation.style.ts
+++ src/components/backgroundAnimation/backgroundAnimation.style.ts
@@ -1,38 +1,51 @@
-import styled, { keyframes } from "styled-components";
-// const bgImage = import('../../assets/bg1.jpg')
-import bgImage1 from "../../assets/bg1.jpg";
-import bgImage2 from "../../assets/bg2.jpg";
+import styled from "styled-components";
+import bgImage from "../../assets/mainBackground.png";
+import smoke from "../../assets/blackSmoke.png";
 
-const backgroundAnimation = keyframes`
-0%{
-    background-image: url(${bgImage1});
-    background-size: 110% 110%;
-}
-    100%{
-        background-image: url(${bgImage2});
-        background-size: 100% 100%;
-    }
-`;
 export const CountDownPageWrapper = styled.section`
-  position: relative;
   width: 100%;
   height: 100vh;
-  background-image: url(${bgImage1});
+  background-image: url(${bgImage});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   color: #fff;
 
-  animation-name: ${backgroundAnimation};
-  animation-direction: alternate-reverse;
-  animation-play-state: running;
-  animation-timing-function: ease-in-out;
-  animation-duration: 20s;
-  animation-fill-mode: forwards;
-  animation-iteration-count: infinite;
   .opaqueCurtain {
+    display: none;
     width: 100%;
     height: 100%;
     background-color: #1111117a;
   }
+
+  .samuraiInSpotlight {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+`;
+export const SamuraiWrapper = styled.section`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  .samuraiImg {
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .smokeImage {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-image: url(${smoke});
+    background-size: cover;
+    background-position: bottom -400px left 0px;
+    background-repeat: no-repeat;
+  }
+`;
+export const ContentHolder = styled.section`
+  position: absolute;
+  width: 100%;
+  height: 100%;
 `;
--- src/components/backgroundAnimation/backgroundAnimation.tsx
+++ src/components/backgroundAnimation/backgroundAnimation.tsx
@@ -1,9 +1,21 @@
-import { CountDownPageWrapper } from "./backgroundAnimation.style";
+import {
+  ContentHolder,
+  CountDownPageWrapper,
+  SamuraiWrapper,
+} from "./backgroundAnimation.style";
+import samurai from "../../assets/samurai.png";
 
 function BackgroundAnimation({ children }: Readonly<{ children: any }>) {
   return (
     <CountDownPageWrapper>
-      <div className="opaqueCurtain">{children}</div>
+      <SamuraiWrapper>
+        <div className="smokeImage"></div>
+        <img className="samuraiImg" src={samurai} alt="samurai"/>
+      </SamuraiWrapper>
+    
+      <ContentHolder>
+        {children}
+      </ContentHolder>
     </CountDownPageWrapper>
   );
 }
